387d49d0a595109d5656f178e2c8c1806c88c29a
Pete Daukintis <peted70@hotmail.com>
2016-07-30 11:19:52 -0700

    tinkering with date parsing

--- DXHealthBot/DXHealthBot/HEALTH/HealthApiIntentHandler.cs
+++ DXHealthBot/DXHealthBot/HEALTH/HealthApiIntentHandler.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using System.Web;
 using Microsoft.Bot.Connector;
 using DXHealthBot.Model;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 namespace DXHealthBot.HEALTH
 {
@@ -36,42 +38,92 @@ namespace DXHealthBot.HEALTH
             switch (data.intents[0].intent)
             {
                 case "SummariseActivity":
+                    ParseResult<Period> res = null;
+                    DateTime dt = new DateTime();
+                    DateTime start = new DateTime();
                     if (entity != null)
                     {
-
                         var entityTime = (lEntity)data.entities.FirstOrDefault(e =>
                         e.type == "builtin.datetime.time" ||
                         e.type == "builtin.datetime.duration" ||
                         e.type == "builtin.datetime.date");
 
-                        ParseResult<Period> res = null;
-
                         // TODO: parse the time formats correctly...
                         if (entityTime.type == "builtin.datetime.duration")
                         {
-                            res = PeriodPattern.NormalizingIsoPattern.Parse(entityTime.resolution.duration);
+                            if (entityTime.resolution.duration == "PT24H")
+                            {
+                                start = DateTime.Now.AddDays(-1);
+                            }
+                            else
+                            {
+                                res = PeriodPattern.NormalizingIsoPattern.Parse(entityTime.resolution.duration);
+                                var st1 = SystemClock.Instance.GetCurrentInstant().InUtc().LocalDateTime - res.Value;
+                                start = st1.ToDateTimeUnspecified();
+                            }
                         }
                         else if (entityTime.type == "builtin.datetime.time")
                         {
-                            res = PeriodPattern.NormalizingIsoPattern.Parse(entityTime.resolution.time);
+                            var str1 = Regex.Replace(entityTime.resolution.time, "[A-Za-z ]", "");
+                            dt = DateTime.Parse(str1, null, System.Globalization.DateTimeStyles.RoundtripKind);
+                            start = dt;
+                            //res = PeriodPattern.NormalizingIsoPattern.Parse(entityTime.resolution.time);
                         }
                         else if (entityTime.type == "builtin.datetime.date")
                         {
+                            //dt = DateTime.Parse(entityTime.resolution.date, null, System.Globalization.DateTimeStyles.RoundtripKind);
+
+                            //var date = LocalDatePattern.IsoPattern.Parse(entityTime.resolution.date);
+                            //var val = date.Value;
+                            //var st1 = SystemClock.Instance.GetCurrentInstant().InUtc().LocalDateTime - date;
+                            //start = st1.ToDateTimeUnspecified();
+
                             var pattern = LocalDatePattern.CreateWithInvariantCulture("yyyy-MM-dd");
                             LocalDate parseResult = pattern.Parse(entityTime.resolution.date).Value;
+                            start = parseResult.ToDateTimeUnspecified();
                         }
                     }
 
-
-                    //var st = SystemClock.Instance.GetCurrentInstant().InUtc().LocalDateTime - res.Value;
+                    LocalDateTime st; 
+                    if (res != null)
+                        st = SystemClock.Instance.GetCurrentInstant().InUtc().LocalDateTime - res.Value;
 
                     // TODO: Make it work dynamically
+                    //DateTime start = dt;
                     //DateTime start = st.ToDateTimeUnspecified();
-                    DateTime start = DateTime.Now.AddDays(-1);
+                    //DateTime start = DateTime.Now.AddDays(-1);
                     DateTime end = DateTime.Now;
 
-
                     var result = await HealthAPI.GetActivity(token, entityStr, start, end);
+                    var sleep = JsonConvert.DeserializeObject<Sleep>(result);
+
+                    // create a textual summary of sleep in that period...
+                    int num = sleep.itemCount;
+                    if (num <= 0)
+                    {
+                        resultStr = "You didn't track any sleep";
+                        break;
+                    }
+                    var total = sleep.sleepActivities.Sum((a) =>
+                    {
+                        if (a.sleepDuration != null)
+                        {
+                            var dur = PeriodPattern.NormalizingIsoPattern.Parse(a.sleepDuration);
+                            return dur.Value.ToDuration().Ticks;
+                        }
+                        else
+                            return 0;
+                    });
+
+                    var av = total / num;
+                    var sleepSpan = TimeSpan.FromTicks((long)av);
+                    var totalSpan = TimeSpan.FromTicks(total);
+
+                    var avSleepStr = $"{sleepSpan.ToString(@"%h")} hrs {sleepSpan.ToString(@"%m")} mins";
+                    var totalSleepStr = $"{totalSpan.ToString(@"%d")} days {totalSpan.ToString(@"%h")} hrs {totalSpan.ToString(@"%m")} mins";
+
+                    resultStr = $"You have tracked {num} sleeps - average sleep per night {avSleepStr} for a total of {totalSleepStr}";
+
                     ret = true;
                     break;
                     //var entityStr = data.entities.FirstOrDefault(e => e.type == "ActivityType").entity;
